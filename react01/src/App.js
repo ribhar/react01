5f77b05d9c047a42e60ccaeb95b53c0fb9e5dfcf
Ritik <varribhar@gmail.com>
2022-07-15 23:50:41 +0530

    added

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const arr = [
+    {name: "HTC", style: "Square"},
+    {name: "Micromax", style: "Circle"}
+  ]
+  // const arr = [1,2,3,4];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {arr.map(el=>
+     <li className={el.style}>{el.name}</li>
+     )}
     </div>
   );
 }
