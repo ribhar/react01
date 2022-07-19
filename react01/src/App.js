e23210611f70d0650845ebaa5163622e4b30a5eb
Ritik <varribhar@gmail.com>
2022-07-19 22:32:20 +0530

    added

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,18 +1,19 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const arr = [
-    {name: "HTC", style: "Square"},
-    {name: "Micromax", style: "Circle"}
-  ]
-  // const arr = [1,2,3,4];
+//  let counter = 0;
+ const [count,setCount] = useState(0)
 
+ const handlechange = (value)=>{
+  setCount(count + value)
+ }
   return (
-    <div className="App">
-     {arr.map(el=>
-     <li className={el.style}>{el.name}</li>
-     )}
+    <div className='App'>
+      <h3>Counter: {count}</h3>
+     <button onClick={()=>handlechange(1)}>ADD 1</button>
+     <button onClick={()=>handlechange(-1)}>SUB 1</button>
+     <div style={count%2===0? {color:"red"} : {color:"green"}}>Number is {count%2===0? "even" : "odd"}</div>
     </div>
   );
 }
---
+++ react01/src/components/Category.jsx
@@ -0,0 +1,11 @@
+import "./category.css"
+function Category({image, label}){
+    return (
+    <div className="category">
+        <img src={image} alt="" />
+        <b>{label}</b>
+    </div>
+    )
+}
+
+export default Category;
